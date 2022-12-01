132aeed747ac455fc1abdedcd0bb8cd6c382aaeb
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-12-01 19:50:48 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -40,12 +40,10 @@ function calculateVolumeClicked() {
   if (angleA == angleB && angleA == angleC) {
     document.getElementById("answers").innerHTML =
       "You have an equilateral triangle"
-  } 
-  else if (angleA != angleB && angleA != angleC) {
+  } else if (angleA != angleB && angleA != angleC) {
     document.getElementById("answers").innerHTML =
       "You have an Scalene triangle"
-  } 
-  else if (angleA != angleB && angleA == angleC) {
+  } else if (angleA != angleB && angleA == angleC) {
     document.getElementById("answers").innerHTML =
       "You have an Isosceles triangle"
   }
